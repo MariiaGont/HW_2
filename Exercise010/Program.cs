6666a8910abfa115821b06e58d397881db9929c6
Mariia <mari.gontareva@inbox.ru>
2022-11-19 23:58:31 +0700

    Exercise010 added

---
+++ Exercise010/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное 
+// число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("На вход принимается трёхзначное число и выводится вторая цифра этого числа.");
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int length = number.ToString().Length;
+int result = ((number / 10) % 10);
+
+if (length == 3) 
+{
+    Console.WriteLine($"{result} - вторая цифра введённого числа.");
+}
+else
+{
+    Console.WriteLine("Введённое число не является трёхзначным.");
+}
