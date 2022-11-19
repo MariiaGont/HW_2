97820d7681bef655f8dbb341a1a4063823258262
Mariia <mari.gontareva@inbox.ru>
2022-11-20 00:18:25 +0700

    Exercise010 corrected

--- Exercise010/Program.cs
+++ Exercise010/Program.cs
@@ -4,6 +4,7 @@
 Console.WriteLine("На вход принимается трёхзначное число и выводится вторая цифра этого числа.");
 Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
 int length = number.ToString().Length;
 int result = ((number / 10) % 10);
 
